347a7665da29e92e49a0c3e197834dde625e7230
fkmoftakhar <f.k.moftakhar@gmail.com>
2022-10-21 14:27:04 +0200

    Update SuperCharacter.gd

--- gap/SuperCharacter.gd
+++ gap/SuperCharacter.gd
@@ -1,19 +1,13 @@
 #
 # SuperCharacter: Computing supercharacter theories of a finite group
 #
-#! @Chapter Introduction
-#!
-#! SuperCharacter is a package which does some
-#! interesting and cool things
-#!
-#! @Chapter Functionality
-#!
-#!
-#! @Section Example Methods
-#!
-#! This section will describe the example
-#! methods of SuperCharacter
 
-#! @Description
-#!   Insert documentation for your function here
-DeclareGlobalFunction( "SuperCharacter_Example" );
+## <#GAPDoc Label="SuperCharacterTheories">
+## <ManSection>
+## <Func Name="SuperCharacterTheories" Arg="G"/>
+## <Description>
+##   Computes all supercharacter theories of the finite group <A>G</A>.
+## </Description>
+## </ManSection>
+## <#/GAPDoc>
+DeclareGlobalFunction( "SuperCharacterTheories" );
